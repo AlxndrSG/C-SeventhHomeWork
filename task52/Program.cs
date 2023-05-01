c51ead404ed0596040f635318dd42d1ebf3dde38
Alexander <firefighter-911@rambler.ru>
2023-05-01 23:53:23 +0300

    Решение задачи 52

---
+++ task52/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее 
+//арифметическое элементов в каждом столбце.
+
+Console.Clear();
+
+int rows = Prompt("Введите количество строк массива: ", "Ошибка! Введены некорректные данные!");
+int columns = Prompt("Введите количество столбцов массива: ", "Ошибка! Введены некорректные данные!");
+
+int[,] array = GetArray(rows, columns);
+
+PrintArray(array);
+
+ColumnsSum(array);
+
+void ColumnsSum(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0)-1; i++)
+        {
+            sum = (sum + array[i, j]);
+        }
+        sum = Math.Round(sum / rows, 1);
+        Console.Write(sum + "; ");
+    }
+    Console.WriteLine();
+}
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Prompt(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userInput))
+        {
+            return userInput;
+        }
+        else Console.WriteLine(errorMessage);
+    }
+}
+
